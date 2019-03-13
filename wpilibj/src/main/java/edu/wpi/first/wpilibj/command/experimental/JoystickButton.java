24c5700a424726f76327d95371aa80abeb14bba7
Sam Carlberg <sam.carlberg@gmail.com>
2019-03-13 17:31:23 -0400

    Make JoystickButton accept GenericHID

--- wpilibj/src/main/java/edu/wpi/first/wpilibj/command/experimental/JoystickButton.java
+++ wpilibj/src/main/java/edu/wpi/first/wpilibj/command/experimental/JoystickButton.java
@@ -1,15 +1,15 @@
 package edu.wpi.first.wpilibj.command.experimental;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.annotation.Incubating;
 
 @Incubating(since = "2020")
 public class JoystickButton implements Trigger {
 
-  private final Joystick m_joystick;
+  private final GenericHID m_joystick;
   private final int m_buttonNum;
 
-  public JoystickButton(Joystick joystick, int buttonNumber) {
+  public JoystickButton(GenericHID joystick, int buttonNumber) {
     m_joystick = joystick;
     m_buttonNum = buttonNumber;
   }
