6b9f872c2f185241c6aca155148a0b05c2fa168e
Sam Carlberg <sam@slfc.dev>
2024-10-19 10:40:21 -0400

    Add missing exports to commands2 module

--- wpilibNewCommands/src/main/java/module-info.java
+++ wpilibNewCommands/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module wpilib.commands2 {
   requires wpilib.units;
   requires wpilib.wpilibj;
+
+  exports edu.wpi.first.wpilibj2.command;
+  exports edu.wpi.first.wpilibj2.command.button;
+  exports edu.wpi.first.wpilibj2.command.sysid;
 }
